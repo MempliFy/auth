e8610905a73326b86e381063cbf30757884fadda
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-12-28 12:47:21 +0900

    add config.py

---
+++ app/core/config.py
@@ -0,0 +1,17 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    SECRET_KEY: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    DATABASE_URL: str = "sqlite+aiosqlite:///./test.db"
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
+
+
+print(f"SECRET_KEY: {settings.SECRET_KEY}")
+print(f"DATABASE_URL: {settings.DATABASE_URL}")
