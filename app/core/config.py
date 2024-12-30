ca9d87c6b3fb3e515bba20832b9803a919261ffa
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-12-30 15:13:27 +0900

    fix config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,11 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     DATABASE_URL: str = "sqlite+aiosqlite:///./test.db"
+    debug: bool = False
 
     class Config:
         env_file = ".env"
